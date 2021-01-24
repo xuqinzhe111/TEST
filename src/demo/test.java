78f7dd2f37d2aed49ddec6f1109c641cd1db78d9
xqz <1021453305@qq.com>
2021-01-24 21:21:27 +0800

    第一次提交

---
+++ src/demo/test.java
@@ -0,0 +1,12 @@
+package demo;
+
+/**
+ * @Author: Xuqinzhe
+ * @Date : 2021/1/24 - 01 -24 - 21:16
+ * @Description: demo
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("a");
+    }
+}
